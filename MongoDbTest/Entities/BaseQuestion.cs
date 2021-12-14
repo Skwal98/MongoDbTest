3b72ae0f8af5614fe67e46001c2ac7c4c115cf87
a.liaukouski <levkalex98@outlook.com>
2021-12-14 17:40:27 +0300

    Add project files.

---
+++ MongoDbTest/Entities/BaseQuestion.cs
@@ -0,0 +1,29 @@
+﻿namespace MongoDbTest.Entities
+{
+    internal class BaseQuestion
+    {
+        public Guid Id { get; set; }
+        public string? Question { get; set; }
+        public bool IsCorrect { get; set; }
+        public string? Answer { get; set; }
+    }
+
+    internal class InputQuestion: BaseQuestion
+    {
+        public int VerbId { get; set; }
+        public int FormIndex { get; set; }
+    }
+
+    internal class TapQuestion: BaseQuestion
+    {
+        public List<int>? VerbIds { get; set; }
+    }
+
+    internal class SelectQuestion : BaseQuestion
+    {
+        public int VerbId { get; set; }
+        public int FormIndex { get; set; }
+        public List<string>? WrongVerbs { get; set; }
+    }
+
+}
---
+++ MongoDbTest/Program.cs
@@ -0,0 +1,75 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+
+var conventionPack = new ConventionPack { new IgnoreExtraElementsConvention(true) };
+ConventionRegistry.Register("IgnoreExtraElements", conventionPack, type => true);
+var conventionCamelCasePack = new ConventionPack { new CamelCaseElementNameConvention() };
+ConventionRegistry.Register("camelCase", conventionCamelCasePack, t => true);
+
+var connectionString = "mongodb://localhost:27017";
+var databaseName = "EnglishDB";
+
+var client = new MongoClient(connectionString);
+var database = client.GetDatabase(databaseName);
+
+
+//await GetCollectionsNames(client);
+
+var userCol = database.GetCollection<Person>("users");
+
+//userCol.InsertOne(new Person { Name = "vanya2", KKK = 222 });
+
+var filter = new BsonDocument();
+using var cursor = await userCol.FindAsync(filter);
+while (await cursor.MoveNextAsync())
+{
+    var people = cursor.Current;
+    foreach (var doc in people)
+    {
+        Console.WriteLine(doc.Name);
+    }
+}
+
+Console.ReadLine();
+
+static async Task GetDatabaseNames(MongoClient client)
+{
+    using (var cursor = await client.ListDatabasesAsync())
+    {
+        var databaseDocuments = await cursor.ToListAsync();
+        foreach (var databaseDocument in databaseDocuments)
+        {
+            Console.WriteLine(databaseDocument["name"]);
+        }
+    }
+}
+
+static async Task GetCollectionsNames(MongoClient client)
+{
+    using (var cursor = await client.ListDatabasesAsync())
+    {
+        var dbs = await cursor.ToListAsync();
+        foreach (var db in dbs)
+        {
+            Console.WriteLine("Collection in db {0}:", db["name"]);
+
+            IMongoDatabase database = client.GetDatabase(db["name"].ToString());
+
+            using (var collCursor = await database.ListCollectionsAsync())
+            {
+                var colls = await collCursor.ToListAsync();
+                foreach (var col in colls)
+                {
+                    Console.WriteLine(col["name"]);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+public class Person
+{
+    public string Name { get; set; }
+}
